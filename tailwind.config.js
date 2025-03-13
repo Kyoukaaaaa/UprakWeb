0b7b50b5ad6a4612eb100558c7ebb6c37b3c3124
Kyoukaaaaa <yukarifr4@gmail.com>
2025-03-13 21:18:04 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#e11d48',
+        secondary: '#be185d',
+        dark: '#0f172a',
+      },
+      container: {
+        center: true,
+        padding: '16px',
+      },
+      mode: "jit",
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
+
+
